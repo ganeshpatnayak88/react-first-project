5ce1fdd63088831eb004fa4c161fc2407a7c3e2f
ganeshpatnayak88 <mpganes9@gmail.com>
2025-12-06 14:57:34 +0530

    first commit

---
+++ reactapp/day_1/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+          <h1 style={{color:"blue"}}>React Virat </h1>
+          <img src="https://img1.hscicdn.com/image/upload/f_auto,t_ds_w_800,q_50/lsci/db/PICTURES/CMS/348100/348106.jpg" alt="" />
+          <button >84 th hundered loading</button>
+          <p style={{fontSize:"40px"}}>Good morning</p>
+
+      </div>
+    
+        
+    </>
+  )
+}
+
+export default App
